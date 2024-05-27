88d5f185b9157d6f8b8fed29d43e159deb24c7e2
Abdulloh Xo'jayev <xodjaevabdulloh54@gmail.com>
2024-05-27 10:19:25 +0500

    tugadi

---
+++ main.js
@@ -0,0 +1,40 @@
+let h1 = document.createElement("h1");
+
+h1.textContent = "Assalomu Alaykum";
+h1.style = "color:blue; background-color:white; text-align:center; padding: 10px o";
+
+let body = document.querySelector("body");
+
+body.appendChild(h1);
+
+let h2 = document.createElement("h2");
+
+h2.textContent = "Mening ismim Abdulloh";
+h2.style = "color:blue; background-color:white; text-align:center; padding: 10px o";
+body.appendChild(h2);
+
+let h3 = document.createElement("h3");
+
+h3.textContent = "Men Mars IT school da oqiyman";
+h3.style = "color:blue; background-color:white; text-align:center; padding: 10px o";
+body.appendChild(h3);
+
+let h4 = document.createElement("h4");
+
+h4.textContent = "Men Fontend kursida oqiyman";
+h4.style = "color:blue; background-color:white; text-align:center; padding: 10px o";
+body.appendChild(h4);
+
+let h5 = document.createElement("h5");
+
+h5.textContent = "Ustozimiz bizga har kuni vazifa beradilar";
+h5.style = "color:blue; background-color:white; text-align:center; padding: 10px o";
+body.appendChild(h5);
+
+let h6 = document.createElement("h6");
+
+h6.textContent = "Va biza cainlar beradilar";
+h6.style = "color:blue; background-color:white; text-align:center; padding: 10px o";
+body.appendChild(h6);
+
+
